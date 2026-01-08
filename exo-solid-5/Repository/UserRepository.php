9199943edd6e0ec1727395b5d8f937dac739135a
Rumen SIMOV <rs596488@iut21.u-bourgogne.fr>
2026-01-08 09:53:03 +0100

    D as Dependency Inversion Principle

--- exo-solid-5/Repository/UserRepository.php
+++ exo-solid-5/Repository/UserRepository.php
@@ -2,18 +2,39 @@
 
 require_once 'RepositoryInterface.php';
 require_once 'User.php';
+require_once 'DatabaseInterface.php';
 
 class UserRepository implements RepositoryInterface
 {
+    private DatabaseInterface $client;
+
+    public function __construct(DatabaseInterface $client)
+    {
+        $this->client = $client;
+    }
+
     public function getUser(string $userEmail) : User
     {
-        // A faire
-        return new User('A implementer', 'en utilisant un client de base de données');
+        foreach ($this->client->fetchAll() as $user) {
+            $email = is_array($user) ? $user['email'] : $user->email;
+            $full_name = is_array($user) ? $user['full_name'] : $user->full_name;
+
+            if ($email === $userEmail) {
+                return new User($full_name, $email);
+            }
+        }
+
+        throw new Exception("Utilisateur non trouvé : $userEmail");
     }
 
     public function getUsers() : array
     {
-        // A faire
-        return [];
+         $usersArray = [];
+        foreach ($this->client->fetchAll() as $user) {
+            $full_name = is_array($user) ? $user['full_name'] : $user->full_name;
+            $email = is_array($user) ? $user['email'] : $user->email;
+            $usersArray[] = new User($full_name, $email);
+        }
+        return $usersArray;
     }
 }

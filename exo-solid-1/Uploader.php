8a6e980bfde50407f70f0593e5171a042957b8e8
Rumen SIMOV <rs596488@iut21.u-bourgogne.fr>
2026-01-08 08:44:34 +0100

    S as Single Responsibility Principle

--- exo-solid-1/Uploader.php
+++ exo-solid-1/Uploader.php
@@ -1,4 +1,7 @@
 <?php
+require_once 'FileInformation.php';
+require_once 'ExtensionDetector.php';
+require_once 'ImageResizer.php';
 
 class Uploader
 {
@@ -18,9 +21,10 @@ class Uploader
 
     public function uploadFile()
     {
-        if (!in_array($this->type, $this->validTypes)) {
-            $this->error = 'Le fichier ' . $this->name . ' n\'est pas d\'un type valide';
+        $extensionDetector = new ExtensionDetector();
 
+        if (!$extensionDetector->isValidType($this->type)) {
+            $this->error = 'Le fichier ' . $this->name . ' n\'est pas d\'un type valide';
             return false;
         } else {
             return true;
@@ -44,55 +48,19 @@ class Uploader
 
     public function getExtension()
     {
-        return pathinfo($this->name, PATHINFO_EXTENSION);
+        $fileInformation = new FileInformation();
+        return $fileInformation->getExtension($this->name);
     }
 
     public function resize($origin, $destination, $width, $maxHeight)
     {
-        $type = $this->getExtension();
-        $pngFamily = ['PNG', 'png'];
-        $jpegFamily = ['jpeg', 'jpg', 'JPG'];
-        if (in_array($type, $jpegFamily)) {
-            $type = 'jpeg';
-        } elseif (in_array($type, $pngFamily)) {
-            $type = 'png';
-        }
-        $function = 'imagecreatefrom' . $type;
-
-        if (!is_callable($function)) {
-            return false;
-        }
-
-        $image = $function($origin);
-
-        $imageWidth = \imagesx($image);
-        if ($imageWidth < $width) {
-            if (!copy($origin, $destination)) {
-                throw new Exception("Impossible de copier le fichier {$origin} vers {$destination}");
-            }
-        } else {
-            $imageHeight = \imagesy($image);
-            $height = (int) (($width * $imageHeight) / $imageWidth);
-            if ($height > $maxHeight) {
-                $height = $maxHeight;
-                $width = (int) (($height * $imageWidth) / $imageHeight);
-            }
-            $newImage = \imagecreatetruecolor($width, $height);
-
-            if ($newImage !== false) {
-                \imagecopyresampled($newImage, $image, 0, 0, 0, 0, $width, $height, $imageWidth, $imageHeight);
-
-                $function = 'image' . $type;
-
-                if (!is_callable($function)) {
-                    return false;
-                }
-
-                $function($newImage, $destination);
-
-                \imagedestroy($newImage);
-                \imagedestroy($image);
-            }
-        }
+        $imageResizer = new ImageResizer();
+        return $imageResizer->resize(
+            $this->getExtension(),
+            $origin,
+            $destination,
+            $width,
+            $maxHeight
+        );
     }
 }
